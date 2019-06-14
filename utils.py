1a07a9bbaaf54ae01d20b20d2aac1348e9882150
wayne1123 <32883157+wayne1123@users.noreply.github.com>
2019-06-14 10:09:17 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -119,7 +119,7 @@ def load_data(dataset_str):
                 objects.append(pkl.load(f))
 
     x, y, tx, ty, allx, ally, graph = tuple(objects)
-    test_idx_reorder = parse_index_file("data/ind.{}.test.index".format(dataset_str))
+    test_idx_reorder = parse_index_file("dataset/ind.{}.test.index".format(dataset_str))
     test_idx_range = np.sort(test_idx_reorder)
 
     if dataset_str == 'citeseer':
